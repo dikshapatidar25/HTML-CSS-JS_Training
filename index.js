c2db8b31056c25d9297c7c084c0f99197b5eec2a
Diksha Patidar <dikshapatidar32@gmail.com>
2025-09-05 20:03:59 +0530

    added conversion

--- index.js
+++ index.js
@@ -331,4 +331,82 @@
 // console.log(notDeclaredVar); // -> Uncaught ReferenceError:
 
 // (NULL)
+// let someResource;
+// console.log(someResource); // -> undefined
+// console.log(typeof someResource); // -> undefined
+// someResource = null;
+// console.log(someResource); // -> null
+// console.log(typeof someResource); // -> object
 
+// TYPE CONVERSION
+// const str = String()
+// const num = Number()
+// const bool = Boolean()
+// console.log(str) // ->
+// console.log("Type of str:",typeof str)// -> 42n
+
+// console.log(num) // -> 0
+// console.log("Type of num:",typeof num)// -> 42n
+
+// console.log(bool) // -> false
+// console.log("Type of bool:",typeof bool) // -> 42n
+
+// const big1 = BigInt(42)
+// console.log(big1)
+// console.log("Type of big1:",typeof big1) // -> 42n
+// const big2 = BigInt() // -> Uncaught TypeError: Cannot convert undefined to a BigInt
+
+// Const num = 42
+// console.log(("num:",num))
+// const strFromNum1 = String(num)
+// console.log("type of strFromNum1:",strFromNum1)
+
+// const strFromNum2 = String(8)
+// const strFromBool = String(true)
+// const numFromStr = Number("312")
+// const boolFromNumber = Boolean(0)
+
+// CONVRT to NUMBER
+// console.log(Number(42)); // -> 42
+// console.log(Number("11")); // -> 11
+// console.log(Number("0x11")); // -> 17
+// console.log(Number("0o11")); // -> 9
+// console.log(Number("0b11")); // -> 3
+// console.log(Number("12e3")); // -> 12000
+// console.log(Number("Infinity"));// -> Infinity
+// console.log(Number("text")); // -> NaN
+// console.log(Number(14n)); // -> 14
+// console.log(Number(123456789123456789123n)); // - > 123456789123
+// console.log(Number(true)); // -> 1
+// console.log(Number(false)); // -> 0
+// console.log(Number(undefined)); // -> NaN
+// console.log(Number(null));// -> 0`
+
+// Convert to Boolean
+// console.log(Boolean(true)); // -> true
+// console.log(Boolean(42)); // -> true
+// console.log(Boolean(0)); // -> false
+// console.log(Boolean(NaN)); // -> false
+// console.log(Boolean("text")); // -> true
+// console.log(Boolean("")); // -> false
+// console.log(Boolean(undefined)); // -> false
+// console.log(Boolean(null)); // -> false
+
+// Convert to BigInt
+// console.log(BigInt(11)); // -> 11n
+// console.log(BigInt(0x11)); // -> 17n
+// console.log(BigInt(11e2)); // -> 1100n
+// console.log(BigInt(true)); // -> 1n
+// console.log(BigInt("11")); // -> 11n
+// console.log(BigInt("0x11")); // -> 17n
+// console.log(BigInt(null)); // -> Uncaught TypeError: Cannot convert null to a BigInt
+// console.log(BigInt(undefined)); // -> Uncaught TypeError: Cannot convert undefined to a BigInt
+// console.log(BigInt(NaN)); // -> Uncaught RangeError: The number NaN cannot
+
+// Implicit Conversions
+const str1 = 42 + "1";
+console.log(str1); // -> 421
+console.log(typeof str1); // -> string
+const str2 = 42 - "2";
+console.log(str2); // -> 41
+console.log(typeof str2); // -> number
