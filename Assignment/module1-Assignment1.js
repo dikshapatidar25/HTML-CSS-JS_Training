76e1309136eae5b8adaeca3e5ba06378901fc0ff
Diksha Patidar <dikshapatidar32@gmail.com>
2025-09-07 18:08:46 +0530

    added module1 assignment1

--- Assignment/module1-Assignment1.js
+++ Assignment/module1-Assignment1.js
@@ -8,19 +8,19 @@ console.log("2003");
 console.log(2003);
 
 // 4. Multiple arguments
-// a.
+ // a.
 console.log("abc", "def", "ghi");
 
-// b. Information about yourself
+ // b. Information about yourself
 console.log("Diksha Patidar (2003)");
 
-// c. All information as one argument
+ // c. All information as one argument
 console.log("Diksha Patidar (2003)");
 
-// d. Name, surname, and year as separate arguments
+ // d. Name, surname, and year as separate arguments
 console.log("Diksha", "Patidar", "(2003)");
 
-// 5. Output on consecutive lines
+ // 5. Output on consecutive lines
 console.log("Diksha");
 console.log("Patidar");
 console.log("2003");
@@ -28,5 +28,5 @@ console.log("2003");
 // 6. Concatenate with +
 console.log("Diksha" + " " + "Patidar" + " " + "(2003)");
 
-// 7. Put spaces correctly using commas
+// 7. Put spaces correctly using commas.
 console.log("Diksha", "Patidar", "(2002)");
