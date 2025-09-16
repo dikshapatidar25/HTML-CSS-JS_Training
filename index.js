947012295405e247c63e441be7676d41499291ce
Diksha Patidar <dikshapatidar32@gmail.com>
2025-09-16 20:04:58 +0530

    Add operator

--- index.js
+++ index.js
@@ -590,6 +590,7 @@
 // console.log(names) 
 
 // concat Method
+
 // let names = ["Olivia", "Emma", "Mateo", "Samuel"];
 // let otherNames = ["William", "Jane"];
 // let allNames = names.concat( otherNames);
@@ -599,6 +600,90 @@
 // "Samuel", "William", "Jane"]
 
 
-let name= ["Payal","Maya","Khuhi"]
-name1= name.concat(name)
-console.log(name1)
+// let name= ["Payal","Maya","Khuhi"]
+// name1= name.concat(name)
+// console.log(name1)
+
+// Module3  Operator 
+// //  Assignment operator
+// let year= 2050;
+// let newyear = year=2051;
+// console.log("year:",year)
+// console.log("newyear:",newyear)
+
+// console.log(2 + 2 * 2)
+// console.log(2 + (2 * 2))
+// console.log((2 + 2) * 2)
+
+// // Arithmetic Operator
+// const x = 5;
+// const y = 2;
+// console.log("addition: ", x + y)
+// console.log("subtraction: ", x - y)
+// console.log("multiplication: ", x * y)
+// console.log("division: ", x / y)
+// console.log("division remainder :", x % y)
+// console.log("exponent: ", x ** y)
+
+// // Unary Arithmetic Operator
+// let str = "123";
+// let n1 = +str
+// let n2 = -str
+// let n3 = -n2;
+// let n4 = +"abcd";
+// console.log(`${str} : ${typeof str}`)
+// console.log(`${n1} : ${typeof n1}`)
+// console.log(`${n2} : ${typeof n2}`)
+// console.log(`${n3} : ${typeof n3}`)
+// console.log(`${n4} : ${typeof n4}`)
+
+// // Unary increment and decrement operators
+// let a= 10
+// console.log("a:",a)
+// console.log("a++:",a++)
+// console.log("a:",a)
+// console.log("--a:",++a)
+
+// console.log("a:",a)
+// console.log("a--:",a--)
+// console.log("a:",a)
+// console.log("--a:",--a)
+
+// console.log(0.2 + 0.1)
+// console.log(0.2 * 0.1)
+// console.log(0.3 / 0.1)
+
+// Compound Assignment operators
+let x = 10;
+x += 2;
+console.log(x)
+x -= 4;
+console.log(x)
+x *= 3;
+console.log(x)
+x /= 6;
+console.log(x)
+x **= 3;
+console.log(x)
+x %= 10;
+console.log(x)
+
+// Logical operators
+// AND 
+console.log(true && true)
+console.log(true && false)
+console.log(false && true)
+console.log(false && false)
+
+// OR
+console.log(true || true)
+console.log(true || false)
+console.log(false || true)
+console.log(false || false)
+
+const a = false;
+const b = true;
+const c = false;
+const d = true;
+console.log(a && b && c || d)
+console.log(a && b && (c || d))
